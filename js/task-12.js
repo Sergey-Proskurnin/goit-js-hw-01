ab65aa6f548803f0833b8a97791ffa275e330fa9
Sergey-Proskurnin <proskurnin@ua.fm>
2020-12-28 02:14:35 +0200

    Добавил 13-18 задачи ДЗ 1

--- js/task-12.js
+++ js/task-12.js
@@ -1,10 +1,10 @@
 function makeOrderMessage(orderedQuantity, pricePerDroid, deliveryFee) {
-    // Пиши код ниже этой строки
+  // Пиши код ниже этой строки
 
-    const totalPrice = (orderedQuantity * pricePerDroid + deliveryFee);
-    const message = (`Вы заказали дроидов на сумму ${totalPrice} кредитов. Доставка (${deliveryFee} кредитов) включена в сумму заказа.`)
-    // Пиши код выше этой строки
-    return message;
+  const totalPrice = orderedQuantity * pricePerDroid + deliveryFee;
+  const message = `Вы заказали дроидов на сумму ${totalPrice} кредитов. Доставка (${deliveryFee} кредитов) включена в сумму заказа.`;
+  // Пиши код выше этой строки
+  return message;
 }
 console.log(makeOrderMessage(2, 100, 50));
 console.log(makeOrderMessage(4, 300, 100));
